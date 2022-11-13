4f3d360078bca3c16422ba3fe01e3774327b38d9
nashEm8 <emanuel82022@outlook.com>
2022-11-12 23:00:45 -0300

    Objetos - Criação e Adição de métodos

---
+++ exercicio-10/app.js
@@ -0,0 +1,163 @@
+/*
+  01
+
+  - Crie um objeto "cat" que possui as seguintes propriedades:
+    - name, que recebe o nome do gato;
+    - age, que recebe a idade;
+    - color, que recebe a cor;
+    - bestFriends, que recebe um array com os melhores amigos do gato;
+    - sound, que é um método que retorna o miado do gato.
+  - Exiba o objeto no console.
+*/
+
+let cat = {
+  name: 'Pelada',
+  age: 2,
+  color: 'Preto e branco',
+  bestFriends: ['Burinecas', 'Branquinho'],
+  sound: function(){
+    return 'Miauuu';
+  }
+}
+
+console.log(cat);
+
+/*
+  02
+
+  - Exiba a mensagem abaixo no console, substituindo os "X" pelas informações  
+    corretas.
+
+  Até aqui, o objeto "cat" possui as seguintes propriedades e valores: "name", que recebeu "X", "age", que recebeu "X", "color", que recebeu "X", "bestFriends", que recebeu um array com os itens "X" e "X", e "sound", que recebeu uma função que retorna "X".
+*/
+
+console.log(`Até aqui, o objeto "cat" possui as seguintes propriedades e valores: "name", que recebeu "${cat.name}", "age", que recebeu "${cat.age}", "color", que recebeu "${cat.color}", "bestFriends", que recebeu um array com os itens "${cat.bestFriends[0]}" e "${cat.bestFriends[1]}", e "sound", que recebeu uma função que retorna "${cat.sound()}".`);
+
+/*
+  03
+
+  - Adicione 2 anos à idade do gato e exiba a idade atualizada no console.
+*/
+
+cat.age += 2;
+console.log(cat.age);
+
+/*
+  04
+
+  - Crie uma função que adiciona um amigo(a) no array de amigos do gato;
+  - Exiba o array de amigos no console para verificar se o novo amigo(a) foi  
+    adicionado.
+*/
+
+function addFriend(name){
+  cat.bestFriends.push(name);
+}
+
+addFriend('Silvérios Brancos');
+console.log(cat.bestFriends);
+
+/*
+  05
+
+  - Crie uma função que adiciona mais uma cor à cor do gato;
+  - Utilize a sintaxe de colchetes para fazer isso;
+  - Exiba a nova cor do gato no console, também utilizando a sintaxe de  
+    colchetes.
+*/
+
+const addColor = (color) => {
+  cat['color'] += color;
+}
+
+addColor('Marron');
+console.log(cat.color);
+
+
+/*
+  06
+
+  - Crie uma função que retorna um boolean indicando se um valor recebido por  
+    parâmetro é um objeto;
+  - Utilize a função para exibir no console se "cat" é um objeto.
+*/
+
+function verifyObject(args){
+  if(typeof args === 'object'){
+    console.log(true);
+  } else {
+    console.log(false)
+  }
+}
+
+verifyObject(cat);
+
+/*
+  07
+
+  - Crie um objeto "dog" que possui as mesmas propriedades do objeto "cat";
+  - Crie uma função que retorna a mensagem abaixo, com as informações corretas;
+  - Exiba a mensagem no console.
+
+  "A soma das idades de NOME_DO_GATO e NOME_DO_CACHORRO é RESULTADO_DA_SOMA."
+*/
+
+let dog = { 
+  name: 'Silvérios Brancos',
+  age: 5,
+  color: 'Branco',
+  bestFriends: ['Dona Pelas', 'Burisburis'],
+  sound: function(){
+    return 'Auau';
+  }
+}
+
+function menssage(){
+  return `A soma das idades de ${cat.name} e ${dog.name} é ${cat.age + dog.age}.`
+}
+
+console.log(menssage());
+
+/*
+  08
+
+  - Se você descomentar os 2 console.log abaixo, verá que a função "isAnSUV"  
+    está funcionando corretamente. Mas ela pode ser melhorada;
+  - Como você refatoraria esta função?
+*/
+const isAnSUV = car => [
+  'Honda HR',
+  'Jeep Renegade',
+  'Ford EcoSport',
+  'Hyundai iX35'
+  ].includes(car);
+
+console.log(isAnSUV('Honda Civic'))
+console.log(isAnSUV('Ford EcoSport'))
+
+/*
+  09
+
+  - Crie uma função que recebe um parâmetro 'type';
+  - Dentro da função, crie um objeto com as seguintes propriedades:
+    - null, que recebe a mensagem 'Seta, explicitamente, uma variável sem valor.'
+    - undefined, que recebe a mensagem 'Representa um valor não-setado.'
+    - object, que recebe a mensagem 'Arrays, Datas, Objetos literais, Funções, etc.'
+  - Se o valor que o parâmetro type receber corresponder à alguma das 3  
+    propriedades, retorne a mensagem que a propriedade armazena;
+  - Teste a função, exibindo no console a mensagem de cada propriedade.
+*/
+
+const myFunction = (type) => {
+  let myObject = {
+    null: 'Seta, explicitamente, uma variável sem valor',
+    undefined: 'Representa um valor não-setado',
+    object: 'Arrays, Datas, Objetos literais, Funções, etc',
+  }
+  
+  return myObject[type];
+}
+
+console.log(myFunction('null'));
+console.log(myFunction('undefined'));
+console.log(myFunction('object'));
